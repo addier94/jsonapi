cc1fef9e340dc19a7659bc4809f2c0f55700e0e7
Alfredo Fernandez <alfredo@hotmail.com>
2021-02-16 22:28:47 -0400

    07 How to order resource part 2

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,31 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasSorts;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSorts;
 
-    /**
-     * The attributes that aren't mass assignable.
-     *
-     * @var array
-     */
+    public $allowedSorts = ['title', 'content'];
+
     protected $guarded = [];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'id' => 'integer',
         'category_id' => 'integer',
         'user_id' => 'integer',
     ];
-
 
     public function category()
     {
