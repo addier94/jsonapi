2fb403eed5f639af207e8431420e1bd21d57c980
Alfredo Fernandez <alfredo@hotmail.com>
2021-02-18 09:46:09 -0400

    10 filter resources

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -6,14 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::applySorts()->jsonPaginate();
+        // $query = Article::query();
+
+        // foreach (request('filter', []) as $filter => $value) {
+        //     if ($filter === 'year') {
+        //         $query->whereYear('created_at', $value);
+        //     } elseif ($filter === 'month') {
+        //         $query->whereMonth('created_at', $value);
+        //     } else {
+        //         $query->where($filter, 'LIKE', "%{$value}%");
+        //     }
+        // }
+
+        // $articles = $query->applySorts()->jsonPaginate();
+        $articles = Article::applyFilters()->applySorts()->jsonPaginate();
         return ArticleCollection::make($articles);
     }
     public function show(Article $article)
